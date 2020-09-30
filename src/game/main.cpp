052a80ad6fc010a1207c5f4ffacb4f9272567d56
ThatGuyGilby <jack.m.gilbride>
2020-09-30 10:52:44 +0100

    Initial Source Code
    Added main.cpp, Core.cpp, Core.h and .gitignore

---
+++ src/game/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <myengine/Core.h>
+
+int main()
+{
+	std::cout << "Hello World" << std::endl;
+	GetVersion();
+	return 0;
+}
---
+++ src/myengine/Core.cpp
@@ -0,0 +1,7 @@
+#include "Core.h"
+#include <iostream>
+
+void GetVersion()
+{
+	std::cout << "Core Version 1.0.1" << std::endl;
+}
